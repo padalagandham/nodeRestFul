b00098b6ccf1b41b9c39245b14095f8be4f3f235
Roop Padala <roop.padala@macys.com>
2017-07-11 15:30:08 -0700

    first push

---
+++ api/routes/todoListRoutes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(app){
+
+	var todoListController = require('../controllers/todoListController');
+
+	app.route('/tasks')
+	.get(todoListController.list_all_tasks)
+	.post(todoListController.create_a_task);
+
+	app.route('/tasks/:taskId')
+	.get(todoListController.read_a_task)
+	.put(todoListController.update_a_task)
+	.delete(todoListController.delete_a_task);
+
+}
+
